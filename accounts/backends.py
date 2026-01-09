f4d58897f266f52822c5ffa49922885b7e163316
ibn-muaz <muazuawe2223@gmail.com>
2026-01-09 23:54:25 +0100

    fix: clean up EmailBackend and finalize login fix

--- accounts/backends.py
+++ accounts/backends.py
@@ -4,11 +4,18 @@ from django.db.models import Q
 
 User = get_user_model()
 
+
 class EmailBackend(ModelBackend):
+    """
+    Custom authentication backend that allows login with email or username.
+    """
     def authenticate(self, request, username=None, password=None, **kwargs):
         # Allow authentication with either username or email
         if username is None:
             username = kwargs.get('email')
+            
+        if username is None:
+            return None
             
         try:
             # Check if username is an email or username
@@ -16,9 +23,10 @@ class EmailBackend(ModelBackend):
         except User.DoesNotExist:
             return None
         except User.MultipleObjectsReturned:
-            # In case multiple users have the same email (should affect unique=True fields but safety first)
-            return User.objects.filter(email=username).order_by('id').first()
+            user = User.objects.filter(email=username).order_by('id').first()
             
         if user.check_password(password) and self.user_can_authenticate(user):
             return user
         return None
+
+
